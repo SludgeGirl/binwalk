b3d562b3476b12ecd1708e630bb7d5478b6ff684
Sludge <alicembrooks@protonmail.com>
2026-04-26 19:54:21 +0100

    Clippy automatic fixes

--- src/json.rs
+++ src/json.rs
@@ -32,8 +32,8 @@ impl JsonLogger {
             ..Default::default()
         };
 
-        if log_file.is_some() {
-            new_instance.json_file = Some(log_file.unwrap().clone());
+        if let Some(log_file) = log_file {
+            new_instance.json_file = Some(log_file);
         }
 
         new_instance
